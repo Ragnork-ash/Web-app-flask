ffe03e7a4ebcca472338acbe631ef13852eebdea
Ragnork-ash <deebhaddad3@hotmail.com>
2024-09-09 13:56:12 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 from website import create_app
 from flask import Flask
+#chk call Flask app
+
 app = create_app()
 
 if __name__ == '__main__':
    # app.run(debug=True)
-    app = Flask(__name__)
+    app.run()
