0e29270a8655dd76e9d945d168ed27770f714d3f
Ragnork-ash <deebhaddad3@hotmail.com>
2024-09-09 14:18:30 +0300

    update main.py add serve from wiatress package to test

--- main.py
+++ main.py
@@ -1,9 +1,14 @@
 from website import create_app
 from flask import Flask
+from waitress import serve
 #chk call Flask app
 
-app = create_app()
+#app = create_app()
+app = Flask(__name__)
 
-if __name__ == '__main__':
+if __name__ == "__main__":
+   serve(app, host="0.0.0.0", port=8000)
+
+
+    #if __name__ == '__main__':
    # app.run(debug=True)
-    app.run()
